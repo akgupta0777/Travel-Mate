90670451c942a675fb7970f139f8d1b0ff61696b
akgupta0777 <akgupta0777@gmail.com>
2021-08-28 19:01:24 +0530

    Updated for security

--- src/App.js
+++ src/App.js
@@ -1,7 +1,33 @@
-import React from "react"
+import React,{useState,useEffect} from "react"
+import Header from "./Components/Header/Header"
+import List from "./Components/List/List"
+import Map from "./Components/Map/Map"
+import PlaceDetails from "./Components/PlaceDetails/PlaceDetails"
+import { CssBaseline, Grid } from "@material-ui/core"
+import { getPlacesData } from "./api"
 
 function App(props){
-    return (<h1>Hello world</h1>);
+    const [place,setPlace] = useState([]);
+    useEffect(() => {
+        getPlacesData().then((data) => {
+            console.log(data);
+            setPlace(data);
+        })
+    },[])
+    return (
+    <>
+    <CssBaseline />
+    <Header />
+    <Grid container spacing={3} style={{width : "100%"}}>
+        <Grid item xs={12} md={4}>
+            <List />
+        </Grid>
+        <Grid item xs={12} md={8}>
+            <Map />
+        </Grid>
+    </Grid>
+    </>
+    );
 }
 
 export default App;
